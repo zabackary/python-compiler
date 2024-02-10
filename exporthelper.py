3af6f431f1a4a93d0009c56e1df2878739f89f38
zabackary <137591653+zabackary@users.noreply.github.com>
2024-02-10 09:52:20 +0900

    update export helper to hopefully work in the environment I'm using it in

--- exporthelper.py
+++ exporthelper.py
@@ -1,43 +1,32 @@
 import ast
 
 EXPORT_HELPER_NAME = "__generated_helper_export__"
+# derived from https://github.com/Infinidat/munch
+# Used to make the locals() dictionary into a attribute-accessible module
+EXPORT_HELPER_CONTENTS = f"""
+class {EXPORT_HELPER_NAME}(dict):
+	C=KeyError
+	A=AttributeError
+	def __init__(A,C):A.update(**C)
+	def __getattr__(B,k):
+		try:return object.__getattribute__(B,k)
+		except A:
+			try:return B[k]
+			except C:raise A(k)
+	def __setattr__(B,k,v):
+		try:object.__getattribute__(B,k)
+		except A:
+			try:B[k]=v
+			except:raise A(k)
+		else:object.__setattr__(B,k,v)
+	def __delattr__(B,k):
+		try:object.__getattribute__(B,k)
+		except A:
+			try:del B[k]
+			except C:raise A(k)
+		else:object.__delattr__(B,k)
+"""
 
 
 def get_export_helper():
-    return ast.ClassDef(
-        name=EXPORT_HELPER_NAME,
-        bases=[],
-        keywords=[],
-        body=[
-            ast.FunctionDef(
-                name='__init__',
-                args=ast.arguments(
-                    posonlyargs=[],
-                    args=[
-                        ast.arg(arg='self'),
-                        ast.arg(arg='entries')
-                    ],
-                    kwonlyargs=[],
-                    kw_defaults=[],
-                    defaults=[]
-                ),
-                body=[
-                    ast.Expr(
-                        value=ast.Call(
-                            func=ast.Attribute(
-                                value=ast.Attribute(value=ast.Name(
-                                    id='self', ctx=ast.Load()), attr='__dict__', ctx=ast.Load()),
-                                attr='update',
-                                ctx=ast.Load()),
-                            args=[
-                                ast.Name(id='entries', ctx=ast.Load())
-                            ],
-                            keywords=[]
-                        )
-                    )
-                ],
-                decorator_list=[]
-            )
-        ],
-        decorator_list=[]
-    )
+    return ast.parse(EXPORT_HELPER_CONTENTS, mode="exec").body[0]
