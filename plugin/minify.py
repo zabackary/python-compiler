18dffe17b0807728a63036755fb6cfcea9bf45be
zabackary <137591653+zabackary@users.noreply.github.com>
2024-02-23 22:17:45 +0900

    add plugins

---
+++ plugin/minify.py
@@ -0,0 +1,35 @@
+from ast import Module, fix_missing_locations, parse, unparse
+from typing import Any
+
+from plugin.plugin import Plugin
+
+from ..python_minifier import minify  # type:ignore
+
+
+class MinifyPlugin(Plugin):
+    minify_kwargs: dict[str, Any]
+
+    def __init__(self, **minify_kwargs) -> None:
+        self.minify_kwargs = minify_kwargs
+        return super().__init__()
+
+    def hook_output(self, module: Module) -> Module:
+        source = unparse(fix_missing_locations(module))
+        source = minify(
+            remove_annotations=True,
+            combine_imports=True,
+            hoist_literals=True,
+            rename_locals=True,
+            rename_globals=True,
+            remove_pass=True,
+            remove_object_base=True,
+            remove_literal_statements=True,
+            constant_folding=True,
+            convert_posargs_to_args=True,
+            remove_explicit_return_none=True,
+            # user overrides
+            **self.minify_kwargs,
+            # input source
+            source=source,
+        )
+        return parse(source)
---
+++ plugin/plugin.py
@@ -0,0 +1,45 @@
+import ast
+
+from processimportedmodule import ModuleUniqueIdentifierGenerator
+from transformer import Import
+
+
+class Plugin:
+    def __init__(self, *args, **kwargs) -> None:
+        pass
+
+    def hook_module(self, module: ast.Module) -> ast.Module:
+        """ A hook run before name translation is performed and modules are bundled """
+        return module
+
+    def hook_module_post_transform(self, module: list[ast.AST], name_generator: ModuleUniqueIdentifierGenerator) -> list[ast.AST]:
+        """ A hook run after name translation is performed but before modules are bundled
+
+        Generally you should not be using this, but it might be useful to perform 
+        some transformation on the exports. It also receives the `ModuleUniqueIdentifierGenerator`
+        instance needed to know how imports and exports are named.
+        """
+        return module
+
+    def hook_import(self, imp: Import) -> Import:
+        """ A hook run on all imports a module imports.
+
+        This can be used to e.g. modify an import path.
+        """
+        return imp
+
+    def hook_import_resolution(self, module: str, context_path: str) -> tuple[str, str] | None:
+        """ A hook run during the module resolution step. 
+
+        This hook is called with the module to be imported's name (`module`) as well
+        as the path it was imported from (`context_path`). It should return either a)
+        a tuple whose first element is the resolved source of the module and second
+        element is the path of the imported module or the string "built-in" if it's
+        generated or a stdlib module or b) None to fall back to the default
+        resolution.
+        """
+        return None
+
+    def hook_output(self, module: ast.Module) -> ast.Module:
+        """ A hook called just prior to the end of code generation. """
+        return module
